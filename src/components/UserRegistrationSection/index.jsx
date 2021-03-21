5d4689f6ac84d61b1c8f8ee9b2ba02c5f7caf475
RastogiAbhijeet <abhijeet.rastogi@block8.com>
2021-03-21 15:19:40 +0530

    Add user registration section

--- src/components/UserRegistrationSection/index.jsx
+++ src/components/UserRegistrationSection/index.jsx
@@ -1,8 +1,27 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
+import LandingSectionContent from "../LandingSectionContent";
+import { styles } from "./styles";
 
-function UserRegistrationSection({content}) {
-  return <Grid style={{ height: "100vh", backgroundColor: "green" }}>{JSON.stringify(content)}</Grid>;
+function UserRegistrationSection({ content }) {
+  const classes = styles();
+  return (
+    <Grid className={classes.container} container alignItems={"stretch"}>
+      <Grid container>
+        <Grid item sm={4}>
+          <LandingSectionContent
+            description={content.description}
+            sectionLogo={content.sectionLogo}
+            sectionTitle={content.sectionTitle}
+            title={content.title}
+          />
+        </Grid>
+        <Grid container item sm={8} justify="center">
+          <img src={content.image}></img>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default UserRegistrationSection;
