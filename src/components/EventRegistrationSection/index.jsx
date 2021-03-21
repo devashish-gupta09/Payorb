850211246422fc1c2b61e3393cfa8241b77d88c0
RastogiAbhijeet <abhijeet.rastogi@block8.com>
2021-03-21 15:28:51 +0530

    Add event registration section on the landing page

--- src/components/EventRegistrationSection/index.jsx
+++ src/components/EventRegistrationSection/index.jsx
@@ -1,9 +1,27 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
+import { styles } from "./styles";
+import LandingSectionContent from "../LandingSectionContent";
 
-function EventRegistrationSection({content}) {
-  
-  return <Grid style={{ height: "100vh", backgroundColor: "green" }}>{JSON.stringify(content)}</Grid>;
+function EventRegistrationSection({ content }) {
+  const classes = styles();
+  return (
+    <Grid className={classes.container} container alignItems={"stretch"}>
+      <Grid container>
+        <Grid item sm={4}>
+          <LandingSectionContent
+            description={content.description}
+            sectionLogo={content.sectionLogo}
+            sectionTitle={content.sectionTitle}
+            title={content.title}
+          />
+        </Grid>
+        <Grid container item sm={8} justify="center">
+          <img src={content.image}></img>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default EventRegistrationSection;
