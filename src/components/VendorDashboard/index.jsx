d34f809fde173a23417839ac3a9cdc8582f7a400
RastogiAbhijeet <abhijeet.rastogi@block8.com>
2021-04-05 21:06:57 +0530

    Uncomment about section on landing page

--- src/components/VendorDashboard/index.jsx
+++ src/components/VendorDashboard/index.jsx
@@ -2,16 +2,18 @@ import { Grid } from "@material-ui/core";
 import React from "react";
 import VendorDashboardHeader from "../DashboardHeader";
 import Profile from "../Profile";
+import VendorDashboardContainer from "../VendorDashboardContainer";
 import { styles } from "./styles";
 
 function VendorDashboard() {
   const classes = styles();
+
   return (
     <Grid>
       <VendorDashboardHeader />
-      <Grid className={classes.container}>
+      <VendorDashboardContainer>
         <Profile />
-      </Grid>
+      </VendorDashboardContainer>
     </Grid>
   );
 }
---
+++ src/components/VendorDashboardContainer/index.jsx
@@ -0,0 +1,10 @@
+import { Grid } from "@material-ui/core";
+import React from "react";
+import { styles } from "./styles";
+
+function VendorDashboardContainer({ children }) {
+  const classes = styles();
+  return <Grid className={classes.container}>{children}</Grid>;
+}
+
+export default VendorDashboardContainer;
