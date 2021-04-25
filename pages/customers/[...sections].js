04722115d12b3dd8fc93c8a96b9a0e6dc86d6502
RastogiAbhijeet <abhijeet.rastogi@block8.com>
2021-04-25 18:30:33 +0530

    Increase event name limit

---
+++ pages/customers/[...sections].js
@@ -0,0 +1,19 @@
+import { Backdrop, CircularProgress, Grid, Typography } from "@material-ui/core";
+import React from 'react';
+
+/**
+ * 
+ * Route Map 
+ * 
+ *  /customer/events : Will show all the events
+ *  /customer/events/register?event=link : Page that shows event details and 
+ *                              a place for registration of event
+ *  /customer/events/review : Page to put event review
+ *  /customer/vendor?userUID=userID 
+ */
+
+export default function Vendor() {
+    return <Grid>
+        
+    </Grid>
+}
--- src/validations/events.js
+++ src/validations/events.js
@@ -3,7 +3,7 @@ import { EVENT_TYPES } from "../constants/events";
 
 
 export const createEventValidationSchema = Yup.object({
-    name: Yup.string().max(40, "Must be 40 characters or less").required(),
+    name: Yup.string().max(100, "Must be 40 characters or less").required(),
     category: Yup.string().required(),
     description: Yup.string().max(200, "Must be 200 characters or less"),
     address: Yup.string().max(100, "Address can't be greater than 100 characters"),
