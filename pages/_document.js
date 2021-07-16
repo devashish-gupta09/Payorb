21b72a898fb495c9cf542f08ae205542697ace2b
RastogiAbhijeet <abhijeet.rastogi@block8.com>
2021-07-16 09:41:49 +0530

    Add meta data for pwa

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,58 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* PWA primary color */}
+
           <meta name="theme-color" content={theme.palette.primary.main} />
           <meta name="referrer" content="origin"></meta>
+
+          <meta name="application-name" content="Payorb" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Payorb" />
+          <meta name="description" content="Payorb" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+
+          <link
+            rel="apple-touch-icon"
+            href="/static/icons/touch-icon-iphone.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/static/icons/touch-icon-ipad.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/icons/touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="/static/icons/touch-icon-ipad-retina.png"
+          />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/assets/logo-mobile.png"
+          />
+
+          <link rel="manifest" href="/manifest.json" />
+
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
 
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
