f1a87a703d9e770dd888cfca71fc5fa01b40beb7
Abhinav <avishwa18@gmail.com>
2021-07-19 16:13:36 +0530

    Add firebase analytics

--- src/utils/ga.js
+++ src/utils/ga.js
@@ -1,12 +1,13 @@
+import firebase from "./firebase";
+
 export const SIGNUP_CLICK = "signup_click";
 export const ABOUT_CLICK = "about_click";
 
 export const pageview = (url) => {
-  window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-    page_path: url,
-  });
+  firebase.analytics().setCurrentScreen(url);
+  firebase.analytics().logEvent("screen_view");
 };
 
 export const event = ({ action, params }) => {
-  window.gtag("event", action, params);
+  firebase.analytics().logEvent(action, params);
 };
