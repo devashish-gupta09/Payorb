01c3c8e74c1a6b4d720e6934ba163d12cc15ca9b
Gaurav Verma <gauravve1@gmail.com>
2021-10-25 00:24:45 +0530

    done

--- src/components/EventsViewList/index.jsx
+++ src/components/EventsViewList/index.jsx
@@ -1,20 +1,47 @@
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, makeStyles, Typography } from "@material-ui/core";
 import React from "react";
+import { isEventPastDate } from "../../utils/events";
 
 import EventCard from "../EventCard";
 
-function EventsViewList({ events, handleEventDelete }) {
+function EventsViewList({ events, handleEventDelete, showOpen }) {
   const classes = styles();
 
   return (
     <Grid container className={classes.root} spacing={4}>
-      {events.map((event, index) => {
-        return (
-          <Grid key={index} item sm={12}>
-            <EventCard event={event} handleEventDelete={handleEventDelete} />
-          </Grid>
-        );
-      })}
+      {showOpen ? (
+        events.filter((e) => !isEventPastDate(e)).length ? (
+          events
+            .filter((e) => !isEventPastDate(e))
+            .map((event, index) => {
+              return (
+                <Grid key={index} item sm={12}>
+                  <EventCard
+                    event={event}
+                    handleEventDelete={handleEventDelete}
+                  />
+                </Grid>
+              );
+            })
+        ) : (
+          <Typography variant="h5">No Open events found...</Typography>
+        )
+      ) : events.filter((e) => isEventPastDate(e)).length ? (
+        events
+          .filter((e) => isEventPastDate(e))
+          .map((event, index) => {
+            return (
+              <Grid key={index} item sm={12}>
+                <EventCard
+                  event={event}
+                  handleEventDelete={handleEventDelete}
+                />
+              </Grid>
+            );
+          })
+      ) : (
+        <Typography variant="h5">No Completed events found...</Typography>
+      )}
     </Grid>
   );
 }
