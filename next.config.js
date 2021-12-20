898cb765c529ddccd9f2b79181cd8dab6b648b9d
Gaurav Verma <gauravve1@gmail.com>
2021-12-20 11:26:13 +0530

    included sentry for client side error capturing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+const { withSentryConfig } = require("@sentry/nextjs");
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withPWA({
+const moduleExports = withPWA({
   pwa: {
     dest: "public",
     runtimeCaching,
@@ -31,3 +32,17 @@ module.exports = withPWA({
     ],
   },
 });
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
