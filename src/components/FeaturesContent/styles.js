29248a8a5c2e519212b0496c20883c8aa662c7a5
ronker-bump <abhijeet.rastogi@bumper.fi>
2022-03-07 21:48:49 +0530

    Fix styling of features page

--- src/components/FeaturesContent/styles.js
+++ src/components/FeaturesContent/styles.js
@@ -3,16 +3,14 @@ import { makeStyles } from "@material-ui/core";
 export const styles = makeStyles((theme) => ({
   container: {
     width: "100vw",
-    border: "2px solid purple",
-    height: "80vh",
-    overflowX: "hidden",
-    margin: "0 auto",
-    justifyContent: "center",
+    height: "fit-content",
+    overflow: "hidden",
     background: "url(/assets/homepage-1.png)",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
+    display: "flex",
     [theme.breakpoints.down('sm')]: {
-      height: "75vh",
+      height: "70vh",
       backgroundRepeat: "no-repeat",
       backgroundSize: "contain",
       backgroundImage: "url(/assets/mobile-home-bg.png)"
@@ -20,11 +18,11 @@ export const styles = makeStyles((theme) => ({
   },
   textContainer: {
     padding: "15em 4em 4em 4em",
-    width: "50%",
+    width: "50%",  
     [theme.breakpoints.down("sm")]: {
       width: "100%",
-      height: "100%",
-      padding: "5em 1em 0 0",
+      height: "fit-content",
+      padding: "5em 0 0 0",
       alignItems: "center",
       display: "flex",
       flexDirection: "column",
@@ -38,7 +36,7 @@ export const styles = makeStyles((theme) => ({
     [theme.breakpoints.down("sm")]: {
       fontSize: "1.6em",
       textAlign: "center",
-      paddingLeft: "1.2em"
+      // paddingLeft: "1.2em"
     },
   },
   titleSection1: {
@@ -72,61 +70,8 @@ export const styles = makeStyles((theme) => ({
     [theme.breakpoints.down("sm")]: {
       width: "100%",
       fontWeight: "500",
-      paddingLeft: "1.2em",
+      // paddingLeft: "1.2em",
       textAlign: "center",
-    },
-  },
-  imgContainer: {
-    display: "inline",
-    position: "absolute",
-    width: "43.875em",
-    height: "29.25em",
-    right: "2.5em",
-    bottom: "0",
-  },
-  image: {
-    width: "90%",
-    bottom: "0",
-    position: "absolute",
-    height: "300px",
-    border: "2px solid green",
-    [theme.breakpoints.down("sm")]: {
-      width: "80vw !important",
-      marginTop: "4rem",
-      padding: "1em 1em",
-    },
-  },
-  buttonContain: {
-    position: "relative",
-    width: "100%",
-    height: "3em",
-    top: "3em",
-    [theme.breakpoints.down("sm")]: {
-      width: "100%",
-      fontWeight: "500",
-      paddingLeft: "1.25em",
-      textAlign: "center",
-    },
-  },
-  imgContainer: {
-    display: "inline",
-    position: "absolute",
-    width: "43.875em",
-    height: "29.25em",
-    right: "2.5em",
-    bottom: "0",
-    [theme.breakpoints.down("sm")]: {
-      height: "20em",
-      width: "100%",
-      right: "0"
-    },
-  },
-  image: {
-    width: "90%",
-    bottom: "0",
-    position: "absolute",
-    [theme.breakpoints.down("sm")]: {
-      width: "400px",
     },
   },
   buttonContain: {
@@ -176,4 +121,21 @@ export const styles = makeStyles((theme) => ({
     position: "relative",
     top: "0.3em",
   },
+  imgContainer: {
+    width: "50vw",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      padding: "1.75em",
+    }
+  },
+  image: {
+    width: "80%",
+    border: "2px solid #00D4FF",
+    [theme.breakpoints.down('sm')]: {
+      width: "100%",
+    }
+  }
 }));
