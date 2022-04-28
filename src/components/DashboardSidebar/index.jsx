791c9199033a2de070f78b4588319ebe2c07fd96
RastogiAbhijeet <abhijeetrastogi1997@gmail.com>
2022-04-28 21:43:27 +0530

    fix: sidebar

--- src/components/DashboardSidebar/index.jsx
+++ src/components/DashboardSidebar/index.jsx
@@ -1,5 +1,4 @@
-import { Box, Drawer, Grid, Tab, Tabs, Typography } from "@material-ui/core";
-import { Close } from "@material-ui/icons";
+import { Box, Grid, Tab, Tabs } from "@material-ui/core";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -24,32 +23,32 @@ function VendorDashboardSidebar({ profileData }) {
     setCurrentTab(value);
     switch (value) {
       case 0:
-        toggleDrawer();
+        // closeDrawer();
         router.push(
           buildVendorDashboardUrl(getVendorIdFromUrl(router), "/events")
         );
         return;
       case 1:
-        closeDrawer();
+        // closeDrawer();
         router.push(
           buildVendorDashboardUrl(getVendorIdFromUrl(router), "/financials")
         );
         return;
       case 2:
-        closeDrawer();
+        // closeDrawer();
         router.push(
           buildVendorDashboardUrl(getVendorIdFromUrl(router), "/customers")
         );
 
         return;
       case 3:
-        closeDrawer();
+        // closeDrawer();
         router.push(
           buildVendorDashboardUrl(getVendorIdFromUrl(router), "/promotions")
         );
         return;
       case 4:
-        closeDrawer();
+        // closeDrawer();
         router.push(
           buildVendorDashboardUrl(getVendorIdFromUrl(router), "/schedule")
         );
@@ -79,146 +78,6 @@ function VendorDashboardSidebar({ profileData }) {
 
   return (
     <Box className={classes.container}>
-      <Drawer
-        anchor="left"
-        open={appMenu}
-        className={classes.drawer}
-        onClose={toggleDrawer}
-      >
-        <Grid className={classes.drawerItemContainer}>
-          <Grid container justify="space-between" alignItems="center">
-            <Typography className={classes.drawerClose} onClick={toggleDrawer}>
-              <Close />
-            </Typography>
-          </Grid>
-          <Box>
-            <Box className={classes.tabs}>
-              <Grid container>
-                <Grid
-                  style={{
-                    width: "50%",
-                    flexGrow: 1,
-                    background: "pink",
-                  }}
-                >
-                  <Tabs
-                    value={currentTab}
-                    onChange={handleTabChange}
-                    textColor="primary"
-                    orientation="vertical"
-                    TabIndicatorProps={{
-                      className: classes.activeTab,
-                    }}
-                  >
-                    <Tab
-                      className={
-                        (isActive("events") && classes.activeLink) ||
-                        classes.btn
-                      }
-                      label={
-                        <div>
-                          <img
-                            src="/assets/sidebar/event-icon.svg"
-                            alt="events"
-                            style={{
-                              verticalAlign: "middle",
-                              paddingRight: "1em",
-                            }}
-                          />
-                          Events
-                        </div>
-                      }
-                      value={0}
-                    />
-                    <Tab
-                      className={
-                        (isActive("financials") && classes.activeLink) ||
-                        classes.btn
-                      }
-                      label={
-                        <div>
-                          <img
-                            src="/assets/sidebar/finance-icon.svg"
-                            alt="financial"
-                            style={{
-                              verticalAlign: "middle",
-                              paddingRight: "1em",
-                            }}
-                          />
-                          Financialshhhhhhh
-                        </div>
-                      }
-                      values={1}
-                    />
-                    <Tab
-                      className={
-                        (isActive("customers") && classes.activeLink) ||
-                        classes.btn
-                      }
-                      label={
-                        <div>
-                          <img
-                            src="/assets/sidebar/customers-icon.svg"
-                            alt="customers"
-                            style={{
-                              verticalAlign: "middle",
-                              paddingRight: "1em",
-                            }}
-                          />
-                          Customers
-                        </div>
-                      }
-                      values={2}
-                    />
-                    <Tab
-                      className={
-                        (isActive("promotions") && classes.activeLink) ||
-                        classes.btn
-                      }
-                      iconPosition="start"
-                      label={
-                        <div>
-                          <img
-                            src="/assets/sidebar/promotion-icon.svg"
-                            alt="promotions"
-                            style={{
-                              verticalAlign: "middle",
-                              paddingRight: "1em",
-                            }}
-                          />
-                          Promotions
-                        </div>
-                      }
-                      values={3}
-                    />
-                    <Tab
-                      className={
-                        (isActive("schedule") && classes.activeLink) ||
-                        classes.btn
-                      }
-                      label={
-                        <div>
-                          <img
-                            src="/assets/sidebar/schedule-icon.svg"
-                            alt="my-schedule"
-                            style={{
-                              verticalAlign: "middle",
-                              paddingRight: "1em",
-                              alignItems: "left",
-                            }}
-                          />
-                          My Schedule
-                        </div>
-                      }
-                      values={4}
-                    />
-                  </Tabs>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Grid>
-      </Drawer>
       {/* For Mobile Screens */}
       <Grid
         container
